9a04f121524b677e13604972364719c4e5f82641
nikhilkumar-infosec <nikhilkumar.infosec@gmail.com>
2026-03-24 10:54:14 +0530

    Update profile.php

--- app/profile.php
+++ app/profile.php
@@ -1,1 +1,30 @@
+<?php
+session_start();
 
+// Check login
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+}
+
+// Logout
+if (isset($_GET['logout'])) {
+    session_destroy();
+    header("Location: login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profile</title>
+</head>
+<body>
+
+<h2>Welcome <?php echo $_SESSION['user']; ?> 😊</h2>
+
+<p>This is your profile page.</p>
+
+<a href="profile.php?logout=true">Logout</a>
+
+</body>
+</html>
