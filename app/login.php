05d3d77da7888249c921259fe7fc5dbf96c12863
nikhilkumar-infosec <nikhilkumar.infosec@gmail.com>
2026-03-24 10:53:53 +0530

    Update login.php

--- app/login.php
+++ app/login.php
@@ -1,1 +1,48 @@
+<?php
+session_start();
 
+// Simple DB connection
+$conn = mysqli_connect("localhost", "phpuser", "", "test_db");
+
+if (!$conn) {
+    die("Connection failed");
+}
+
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Simple query
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) == 1) {
+        $_SESSION['user'] = $username;
+        header("Location: profile.php");
+    } else {
+        echo "Invalid login!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h2>Login Page</h2>
+
+<form method="post">
+    Username:<br>
+    <input type="text" name="username"><br><br>
+
+    Password:<br>
+    <input type="password" name="password"><br><br>
+
+    <input type="submit" name="login" value="Login">
+</form>
+
+</body>
+</html>
